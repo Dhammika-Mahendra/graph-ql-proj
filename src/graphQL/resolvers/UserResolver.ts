5e0b0f5f88c660ff7dd65967f0d3888c81ec9ca0
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-10-02 17:37:16 +0530

    basic test init

---
+++ src/graphQL/resolvers/UserResolver.ts
@@ -0,0 +1,14 @@
+import { Query, Resolver } from "@nestjs/graphql";
+import { User } from "../models/User";
+
+@Resolver()
+export class UserResolver{
+    @Query(returns => User)
+    getUser(){
+        return{
+            id: 1,
+            username: 'john_doe',
+            displayName: 'John Doe'
+        }
+    }
+}
